143551c367e29cca331407bf2302c8394c9c85fb
Cem Saydam <ahmetcemsaydam@gmail.com>
2022-12-13 12:48:11 +0000

    updated the function
    refactored the func.
    
    -fixed some typos

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -2,11 +2,11 @@
 
 export function introduction(person) {
 
-  const {name,ctiy,pet} = person || undefined ;
+  const {name,city,pet} = person ;
 
   // const name = person.name;
   // const city = person.city;
   // const pet = person.pet;
 
-  return "Hello my name is " + name + " and I live in " + city + " with " + pet;
-}
+  return `Hello my name is ${ name } and I live in ${ city } with ${ pet }`;
+};
